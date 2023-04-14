a4c5d49351f93fd45ce4418754d545047650c591
Barmaley <barmaley2005@gmail.com>
2023-04-14 22:10:09 +0300

    first commit

---
+++ bitrix/.settings.php
@@ -0,0 +1,64 @@
+<?php
+return array (
+  'utf_mode' => 
+  array (
+    'value' => true,
+    'readonly' => true,
+  ),
+  'cache_flags' => 
+  array (
+    'value' => 
+    array (
+      'config_options' => 3600.0,
+      'site_domain' => 3600.0,
+    ),
+    'readonly' => false,
+  ),
+  'cookies' => 
+  array (
+    'value' => 
+    array (
+      'secure' => false,
+      'http_only' => true,
+    ),
+    'readonly' => false,
+  ),
+  'exception_handling' => 
+  array (
+    'value' => 
+    array (
+      'debug' => false,
+      'handled_errors_types' => 4437,
+      'exception_errors_types' => 4437,
+      'ignore_silence' => false,
+      'assertion_throws_exception' => true,
+      'assertion_error_type' => 256,
+      'log' => NULL,
+    ),
+    'readonly' => false,
+  ),
+  'connections' => 
+  array (
+    'value' => 
+    array (
+      'default' => 
+      array (
+        'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+        'host' => 'localhost',
+        'database' => 'cy25556_oren',
+        'login' => 'cy25556_oren',
+        'password' => 'GaLg7Eqc',
+        'options' => 2.0,
+      ),
+    ),
+    'readonly' => true,
+  ),
+  'crypto' => 
+  array (
+    'value' => 
+    array (
+      'crypto_key' => '8bd89c24a7138a165678c3eb43dc947b',
+    ),
+    'readonly' => true,
+  ),
+);
---
+++ bitrix/modules/sender/install/version.php
@@ -0,0 +1,5 @@
+<?php
+$arModuleVersion = [
+	'VERSION' => '22.300.0',
+	'VERSION_DATE' => '2022-08-29 15:01:33'
+];
